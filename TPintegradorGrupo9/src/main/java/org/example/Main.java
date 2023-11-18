df09d1b0c80f6ad6f14d2c6bed1cf27a6db0d54b
Marcos <125236825+Marktaur@users.noreply.github.com>
2023-11-18 09:37:42 -0300

    Actualizar Main.java

--- TPintegradorGrupo9/src/main/java/org/example/Main.java
+++ TPintegradorGrupo9/src/main/java/org/example/Main.java
@@ -2,19 +2,30 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-    Cliente Macdonals;
-
-    {
-        Macdonals = new Cliente(1, "Arcos Dorados S.a", "3044545455", "tango");
-    }
-
-    Incidente primerIncidente = new
-
-    // como hago para crear un cliente o un incidente cuando ya tengo creado anteriormente
-    //deberia avisar de alguna manera cual es el ultimo o antes de crear tirar un listado para no pisar
 
 
+        System.out.println("Bienvenido al Sistema de gestión grupo 9");
+ try (.  // iniciamos una posible excepción a la operatibilidad del programa.
+
+Do ( 
+
+    /*acá deberíamos mostrar un menú que cuente con
+opciónes y podemos elejirlos con scanner.in:
+Clientes,recursos humanos, tipos de problema , soluciones, técnicos, tipos de servicios,
+1) clientes debería iniciar el menú mostrando los clientes ya cargados.utilizando tostring en forma de cuadriculas prefija para todos las vistas del programa. 
+El próximo cliente a cargar debería ser el primer ID disponible. 
+Dentro de los datos a modificar o crear debería tener que solicitar que servicios hay disponibles para luego seleccionar uno. 
+
+2) el menú mesa de ayuda o incidentes debería empezar mostrándo los incidentes con sus datos y estados con tostring.
+y luego permitir sus distintos cambios. 
+
+3)recursos humanos debería contener laas opciones de reportes y las altas de los técnicos. 
+
+tipo de problema  y soluciones también deben contener laa opciones de visualizar ,alta, baja y modificación. 
+
+
+
+  
+
+*/
 }
